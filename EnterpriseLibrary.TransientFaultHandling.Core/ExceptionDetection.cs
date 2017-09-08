408146c481715ba16a6f406d6be34f6c7f2b2e0e
Dixin Yan <dixinyan@live.com>
2017-09-07 22:27:20 -0700

    Add XML comments to new APIs.

--- EnterpriseLibrary.TransientFaultHandling.Core/ExceptionDetection.cs
+++ EnterpriseLibrary.TransientFaultHandling.Core/ExceptionDetection.cs
@@ -2,19 +2,38 @@
 {
     using System;
 
+    /// <summary>
+    /// Detects specific transient conditions.
+    /// </summary>
     public class ExceptionDetection : ITransientErrorDetectionStrategy
     {
         private readonly Func<Exception, bool> isTransient;
 
-        public ExceptionDetection(Func<Exception, bool> isTransient = null) => 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionDetection"/> class.
+        /// </summary>
+        /// <param name="isTransient">The predicate function to detect whether the specified exception is transient.</param>
+        public ExceptionDetection(Func<Exception, bool> isTransient = null) =>
             this.isTransient = isTransient ?? (_ => true);
 
+        /// <summary>
+        /// Determines whether the specified exception is transient.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        /// <returns><c>true</c> if the specified exception is transient; otherwise, <c>false</c>.</returns>
         public bool IsTransient(Exception exception) => this.isTransient(exception);
     }
 
+    /// <summary>
+    /// Detects specific transient exception.
+    /// </summary>
+    /// <typeparam name="TException">The type of the transient exception.</typeparam>
     public class TransientDetection<TException> : ExceptionDetection
         where TException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TransientDetection{TException}"/> class.
+        /// </summary>
         public TransientDetection() : base(exception => exception is TException)
         {
         }
--- EnterpriseLibrary.TransientFaultHandling.Core/Retry.cs
+++ EnterpriseLibrary.TransientFaultHandling.Core/Retry.cs
@@ -1,11 +1,23 @@
 ﻿namespace Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling
 {
     using System;
-    using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// Provides the retry methods.
+    /// </summary>
     public static class Retry
     {
+        /// <summary>
+        /// Repetitively executes the specified action while it satisfies the specified retry strategy.
+        /// </summary>
+        /// <typeparam name="TResult">he type of result expected from the executable action.</typeparam>
+        /// <param name="func">A delegate that represents the executable action that returns the result of type <typeparamref name="TResult" />.</param>
+        /// <param name="retryStrategy">The retry strategy.</param>
+        /// <param name="isTransient">The predicate function to detect whether the specified exception is transient.</param>
+        /// <param name="retryingHandler">The callback function that will be invoked whenever a retry condition is encountered.</param>
+        /// <returns>The result from the action.</returns>
+        /// <exception cref="ArgumentNullException">func</exception>
         public static TResult Execute<TResult>(
             Func<TResult> func,
             RetryStrategy retryStrategy = null,
@@ -27,14 +39,25 @@
             return retryPolicy.ExecuteAction(func);
         }
 
+        /// <summary>
+        /// Repetitively executes the specified action while it satisfies the specified retry strategy.
+        /// </summary>
+        /// <typeparam name="TResult">he type of result expected from the executable action.</typeparam>
+        /// <param name="func">A delegate that represents the executable action that returns the result of type <typeparamref name="TResult" />.</param>
+        /// <param name="retryCount">The number of retry attempts.</param>
+        /// <param name="isTransient">The predicate function to detect whether the specified exception is transient.</param>
+        /// <param name="retryingHandler">The callback function that will be invoked whenever a retry condition is encountered.</param>
+        /// <param name="retryInterval">The time interval between retries.</param>
+        /// <param name="firstFastRetry">true to immediately retry in the first attempt; otherwise, false. The subsequent retries will remain subject to the configured retry interval.</param>
+        /// <returns>The result from the action.</returns>
+        /// <exception cref="ArgumentNullException">func</exception>
         public static TResult Execute<TResult>(
             Func<TResult> func,
             int? retryCount = null,
             Func<Exception, bool> isTransient = null,
             EventHandler<RetryingEventArgs> retryingHandler = null,
             TimeSpan? retryInterval = null,
-            bool? firstFastRetry = null,
-            [CallerMemberName] string name = null)
+            bool? firstFastRetry = null)
         {
             if (func == null)
             {
@@ -43,21 +66,37 @@
 
             return Execute(
                 func,
-                FixedInterval(retryCount, retryInterval, firstFastRetry, name),
+                FixedInterval(retryCount, retryInterval, firstFastRetry),
                 isTransient,
                 retryingHandler);
         }
 
+        /// <summary>
+        /// Create a new instance of the <see cref="T:Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling.FixedInterval" /> class. 
+        /// </summary>
+        /// <param name="retryCount">The number of retry attempts.</param>
+        /// <param name="retryInterval">The time interval between retries.</param>
+        /// <param name="firstFastRetry">true to immediately retry in the first attempt; otherwise, false. The subsequent retries will remain subject to the configured retry interval.</param>
+        /// <param name="name">The retry strategy name.</param>
+        /// <returns>A new instance of the <see cref="T:Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling.FixedInterval" /> class.</returns>
         public static FixedInterval FixedInterval(
             int? retryCount = null,
             TimeSpan? retryInterval = null,
             bool? firstFastRetry = null,
-            [CallerMemberName] string name = null) => new FixedInterval(
+            string name = null) => new FixedInterval(
             name,
             retryCount ?? RetryStrategy.DefaultClientRetryCount,
             retryInterval ?? RetryStrategy.DefaultRetryInterval,
             firstFastRetry ?? RetryStrategy.DefaultFirstFastRetry);
 
+        /// <summary>
+        /// Repetitively executes the specified action while it satisfies the specified retry strategy.
+        /// </summary>
+        /// <param name="action">A delegate that represents the executable action that doesn't return any results.</param>
+        /// <param name="retryStrategy">The retry strategy.</param>
+        /// <param name="isTransient">The predicate function to detect whether the specified exception is transient.</param>
+        /// <param name="retryingHandler">The callback function that will be invoked whenever a retry condition is encountered.</param>
+        /// <exception cref="ArgumentNullException">action</exception>
         public static void Execute(
             Action action,
             RetryStrategy retryStrategy = null,
@@ -79,14 +118,23 @@
             retryPolicy.ExecuteAction(action);
         }
 
+        /// <summary>
+        /// Repetitively executes the specified action while it satisfies the specified retry strategy.
+        /// </summary>
+        /// <param name="action">A delegate that represents the executable action that doesn't return any results.</param>
+        /// <param name="retryCount">The number of retry attempts.</param>
+        /// <param name="isTransient">The predicate function to detect whether the specified exception is transient.</param>
+        /// <param name="retryingHandler">The callback function that will be invoked whenever a retry condition is encountered.</param>
+        /// <param name="retryInterval">The time interval between retries.</param>
+        /// <param name="firstFastRetry">true to immediately retry in the first attempt; otherwise, false. The subsequent retries will remain subject to the configured retry interval.</param>
+        /// <exception cref="ArgumentNullException">action</exception>
         public static void Execute(
             Action action,
             int? retryCount = null,
             Func<Exception, bool> isTransient = null,
             EventHandler<RetryingEventArgs> retryingHandler = null,
             TimeSpan? retryInterval = null,
-            bool? firstFastRetry = null,
-            [CallerMemberName] string name = null)
+            bool? firstFastRetry = null)
         {
             if (action == null)
             {
@@ -95,32 +143,21 @@
 
             Execute(
                 action,
-                FixedInterval(retryCount, retryInterval, firstFastRetry, name),
+                FixedInterval(retryCount, retryInterval, firstFastRetry),
                 isTransient,
                 retryingHandler);
         }
 
-        public static Task<TResult> ExecuteAsync<TResult>(
-            Func<Task<TResult>> func,
-            int? retryCount = null,
-            Func<Exception, bool> isTransient = null,
-            EventHandler<RetryingEventArgs> retryingHandler = null,
-            TimeSpan? retryInterval = null,
-            bool? firstFastRetry = null,
-            [CallerMemberName] string name = null)
-        {
-            if (func == null)
-            {
-                throw new ArgumentNullException(nameof(func));
-            }
-
-            return ExecuteAsync(
-                func,
-                FixedInterval(retryCount, retryInterval, firstFastRetry, name),
-                isTransient,
-                retryingHandler);
-        }
-
+        /// <summary>
+        /// Repeatedly executes the specified asynchronous function while it satisfies the current retry policy.
+        /// </summary>
+        /// <typeparam name="TResult">he type of result expected from the executable asynchronous function.</typeparam>
+        /// <param name="func">A asynchronous function that returns a started task (also known as "hot" task).</param>
+        /// <param name="retryStrategy">The retry strategy.</param>
+        /// <param name="isTransient">The predicate function to detect whether the specified exception is transient.</param>
+        /// <param name="retryingHandler">The callback function that will be invoked whenever a retry condition is encountered.</param>
+        /// <returns>Returns a task that will run to completion if the original task completes successfully (either the first time or after retrying transient failures). If the task fails with a non-transient error or the retry limit is reached, the returned task will transition to a faulted state and the exception must be observed.</returns>
+        /// <exception cref="ArgumentNullException">func</exception>
         public static Task<TResult> ExecuteAsync<TResult>(
             Func<Task<TResult>> func,
             RetryStrategy retryStrategy = null,
@@ -142,14 +179,25 @@
             return retryPolicy.ExecuteAsync(func);
         }
 
-        public static Task ExecuteAsync(
-            Func<Task> func,
+        /// <summary>
+        /// Repeatedly executes the specified asynchronous function while it satisfies the current retry policy.
+        /// </summary>
+        /// <typeparam name="TResult">he type of result expected from the executable asynchronous function.</typeparam>
+        /// <param name="func">A asynchronous function that returns a started task (also known as "hot" task).</param>
+        /// <param name="retryCount">The number of retry attempts.</param>
+        /// <param name="isTransient">The predicate function to detect whether the specified exception is transient.</param>
+        /// <param name="retryingHandler">The callback function that will be invoked whenever a retry condition is encountered.</param>
+        /// <param name="retryInterval">The time interval between retries.</param>
+        /// <param name="firstFastRetry">true to immediately retry in the first attempt; otherwise, false. The subsequent retries will remain subject to the configured retry interval.</param>
+        /// <returns>Returns a task that will run to completion if the original task completes successfully (either the first time or after retrying transient failures). If the task fails with a non-transient error or the retry limit is reached, the returned task will transition to a faulted state and the exception must be observed.</returns>
+        /// <exception cref="ArgumentNullException">func</exception>
+        public static Task<TResult> ExecuteAsync<TResult>(
+            Func<Task<TResult>> func,
             int? retryCount = null,
             Func<Exception, bool> isTransient = null,
             EventHandler<RetryingEventArgs> retryingHandler = null,
             TimeSpan? retryInterval = null,
-            bool? firstFastRetry = null,
-            [CallerMemberName] string name = null)
+            bool? firstFastRetry = null)
         {
             if (func == null)
             {
@@ -158,11 +206,20 @@
 
             return ExecuteAsync(
                 func,
-                FixedInterval(retryCount, retryInterval, firstFastRetry, name),
+                FixedInterval(retryCount, retryInterval, firstFastRetry),
                 isTransient,
                 retryingHandler);
         }
 
+        /// <summary>
+        /// Repeatedly executes the specified asynchronous function while it satisfies the current retry policy.
+        /// </summary>
+        /// <param name="func">A asynchronous function that returns a started task (also known as "hot" task).</param>
+        /// <param name="retryStrategy">The retry strategy.</param>
+        /// <param name="isTransient">The predicate function to detect whether the specified exception is transient.</param>
+        /// <param name="retryingHandler">The callback function that will be invoked whenever a retry condition is encountered.</param>
+        /// <returns>Returns a task that will run to completion if the original task completes successfully (either the first time or after retrying transient failures). If the task fails with a non-transient error or the retry limit is reached, the returned task will transition to a faulted state and the exception must be observed.</returns>
+        /// <exception cref="ArgumentNullException">func</exception>
         public static Task ExecuteAsync(
             Func<Task> func,
             RetryStrategy retryStrategy = null,
@@ -182,6 +239,37 @@
             }
 
             return retryPolicy.ExecuteAsync(func);
+        }
+
+        /// <summary>
+        /// Repeatedly executes the specified asynchronous function while it satisfies the current retry policy.
+        /// </summary>
+        /// <param name="func">A asynchronous function that returns a started task (also known as "hot" task).</param>
+        /// <param name="retryCount">The number of retry attempts.</param>
+        /// <param name="isTransient">The predicate function to detect whether the specified exception is transient.</param>
+        /// <param name="retryingHandler">The callback function that will be invoked whenever a retry condition is encountered.</param>
+        /// <param name="retryInterval">The time interval between retries.</param>
+        /// <param name="firstFastRetry">true to immediately retry in the first attempt; otherwise, false. The subsequent retries will remain subject to the configured retry interval.</param>
+        /// <returns>Returns a task that will run to completion if the original task completes successfully (either the first time or after retrying transient failures). If the task fails with a non-transient error or the retry limit is reached, the returned task will transition to a faulted state and the exception must be observed.</returns>
+        /// <exception cref="ArgumentNullException">func</exception>
+        public static Task ExecuteAsync(
+            Func<Task> func,
+            int? retryCount = null,
+            Func<Exception, bool> isTransient = null,
+            EventHandler<RetryingEventArgs> retryingHandler = null,
+            TimeSpan? retryInterval = null,
+            bool? firstFastRetry = null)
+        {
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+
+            return ExecuteAsync(
+                func,
+                FixedInterval(retryCount, retryInterval, firstFastRetry),
+                isTransient,
+                retryingHandler);
         }
     }
 }
