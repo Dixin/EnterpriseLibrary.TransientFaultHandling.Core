33df15b59547aedd8afbdcc545f887373e6e0c42
Dixin <dixinyan@live.com>
2021-11-16 02:03:20 -0800

    Add more code error code. Fix #4.

---
+++ Source/TransientFaultHandling.Data.Core/Data/ProcessNetLibErrorCode.cs
@@ -0,0 +1,55 @@
+﻿namespace Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling.Data;
+
+/// <summary>
+/// Error codes reported by the DBNETLIB module.
+/// </summary>
+internal enum ProcessNetLibErrorCode
+{
+    ZeroBytes = -3,
+
+    Timeout = -2, /* Timeout expired. The timeout period elapsed prior to completion of the operation or the server is not responding. */
+
+    Unknown = -1,
+
+    InsufficientMemory = 1,
+
+    AccessDenied = 2,
+
+    ConnectionBusy = 3,
+
+    ConnectionBroken = 4,
+
+    ConnectionLimit = 5,
+
+    ServerNotFound = 6,
+
+    NetworkNotFound = 7,
+
+    InsufficientResources = 8,
+
+    NetworkBusy = 9,
+
+    NetworkAccessDenied = 10,
+
+    GeneralError = 11,
+
+    IncorrectMode = 12,
+
+    NameNotFound = 13,
+
+    InvalidConnection = 14,
+
+    ReadWriteError = 15,
+
+    TooManyHandles = 16,
+
+    ServerError = 17,
+
+    SSLError = 18,
+
+    EncryptionError = 19,
+
+    EncryptionNotSupported = 20
+}
+
+// this is a specific error class we need to look for see http://technet.microsoft.com/en-us/library/aa937483(v=SQL.80).aspx
---
+++ Source/TransientFaultHandling.Data.Core/SqlAzureTransientErrorDetectionStrategyLegacy.cs
@@ -0,0 +1,20 @@
+﻿namespace Microsoft.Practices.EnterpriseLibrary.WindowsAzure.TransientFaultHandling.SqlAzure;
+
+using Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling;
+
+/// <summary>
+/// This class is obsolete. Please use <see cref="SqlDatabaseTransientErrorDetectionStrategy"/>.
+/// Provides the transient error detection logic for transient faults that are specific to SQL Database.
+/// </summary>
+[Obsolete("Use SqlDatabaseTransientErrorDetectionStrategy for Microsoft.Data.SqlClient.", false)]
+public class SqlAzureTransientErrorDetectionStrategyLegacy : ITransientErrorDetectionStrategy
+{
+    private readonly SqlDatabaseTransientErrorDetectionStrategyLegacy inner = new();
+
+    /// <summary>
+    /// Determines whether the specified exception represents a transient failure that can be compensated by a retry.
+    /// </summary>
+    /// <param name="ex">The exception object to be verified.</param>
+    /// <returns>true if the specified exception is considered transient; otherwise, false.</returns>
+    public bool IsTransient(Exception ex) => this.inner.IsTransient(ex);
+}
