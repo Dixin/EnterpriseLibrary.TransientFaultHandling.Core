3fefaf78a35f1a6de2b1ca13de99b92e240a4cf7
Dixin <dixinyan@live.com>
2021-11-16 02:56:35 -0800

    Use the old security rule for .NET Sandard build, so that it can be used by .NET Framework projects. Fix #5.

---
+++ Tests/TransientFaultHandling.Bvt.Tests/Sql/SqlExceptionTests.cs
@@ -0,0 +1,67 @@
+﻿namespace Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling.Bvt.Tests.Sql;
+
+[TestClass]
+public class SqlExceptionTests
+{
+    private const string CustomErrorMessage = nameof(CustomErrorMessage);
+
+    [TestMethod]
+    public async Task CanHandleSqlExceptionAsync()
+    {
+        RetryPolicy<SqlDatabaseTransientErrorDetectionStrategy> retryPolicy = new (new Incremental(RetryStrategy.DefaultClientRetryCount, RetryStrategy.DefaultRetryInterval, RetryStrategy.DefaultRetryIncrement));
+
+        try
+        {
+            await retryPolicy.ExecuteAsync(async () =>
+            {
+                await using SqlConnection connection = new (TestDatabase.TransientFaultHandlingTestDatabase);
+                await connection.OpenAsync();
+                await using SqlCommand? command = connection.CreateCommand();
+                command.CommandText = $"RAISERROR('{CustomErrorMessage}', 16, 1)";
+                await command.ExecuteNonQueryAsync();
+                Assert.Fail();
+            });
+        }
+        catch (Exception ex)
+        {
+            if (ex is SqlException { Message: CustomErrorMessage })
+            {
+                return;
+            }
+
+            Assert.Fail();
+        }
+
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void CanHandleSqlException()
+    {
+        RetryPolicy<SqlDatabaseTransientErrorDetectionStrategy> retryPolicy = new (new Incremental(RetryStrategy.DefaultClientRetryCount, RetryStrategy.DefaultRetryInterval, RetryStrategy.DefaultRetryIncrement));
+
+        try
+        {
+            retryPolicy.ExecuteAction(() =>
+            {
+                using SqlConnection connection = new (TestDatabase.TransientFaultHandlingTestDatabase);
+                connection.Open();
+                using SqlCommand? command = connection.CreateCommand();
+                command.CommandText = $"RAISERROR('{CustomErrorMessage}', 16, 1)";
+                command.ExecuteNonQuery();
+                Assert.Fail();
+            });
+        }
+        catch (Exception ex)
+        {
+            if (ex is SqlException { Message: CustomErrorMessage })
+            {
+                return;
+            }
+
+            Assert.Fail();
+        }
+
+        Assert.Fail();
+    }
+}
--- Tools/AssemblyInfo.cs
+++ Tools/AssemblyInfo.cs
@@ -2,3 +2,12 @@
 
 [assembly: CLSCompliant(true)]
 [assembly: SecurityTransparent]
+
+#if NETSTANDARD1_6 || NETSTANDARD2_0
+// The NETSTANDARD1_6 and NETSTANDARD2_0 builds can be used by .NET Framework projects.
+// Without this, when TransientFaultHandling.Data.Core is used by .NET Framework projects, it throws System.TypeAccessException when handling SqlException:
+// Attempt by security transparent method 'Microsoft.Practices.EnterpriseLibrary.TransientFaultHandling.SqlDatabaseTransientErrorDetectionStrategy.IsTransient(System.Exception)' to access security critical type 'Microsoft.Data.SqlClient.SqlException' failed.
+// NETSTANDARD1_0 to NETSTANDARD1_5 does not support [SecurityRules]. And NETSTANDARD2_1_OR_GREATER is not supported by .NET Framework.
+// https://docs.microsoft.com/en-us/dotnet/standard/net-standard#specification
+[assembly: SecurityRules(SecurityRuleSet.Level1)]
+#endif
